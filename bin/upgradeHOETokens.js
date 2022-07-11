85c1b889191b05f2e0944a49b468614b88600331
goldendilemma <goldendilemma@users.noreply.github.com>
2022-07-11 16:48:31 +0200

    ✨ Upgrade script for HOE

---
+++ bin/upgradeHOETokens.js
@@ -0,0 +1,46 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+async function start ([inputDir, newAnimationUrl]) {
+  const files = fs.readdirSync(inputDir)
+    .filter(filename => filename.slice(-4) === 'json')
+    .map(filename => path.join(inputDir, filename))
+  
+  for (const file of files) {
+    const content = fs.readJSONSync(file)
+    const { 
+      animation_url,
+      ...filtered 
+    } = content
+    const newAnimation = upgradeUrl(animation_url, newAnimationUrl)
+    const updated = {
+      ...filtered,
+      animation_url: newAnimation
+    }
+    fs.writeJSONSync(file, updated)
+    console.log('updated', file)
+  }
+}
+
+const getQuery = (url) => {
+  const parts = url.split('?')
+  if (parts.length > 1) {
+    const query = parts[parts.length - 1]
+    return query
+  } else {
+    return null
+  }
+}
+
+const upgradeUrl = (fullUrl, newUrl) => {
+  const parts = fullUrl.split('?')
+  if (parts.length > 1) {
+    const query = parts[parts.length - 1]
+    return `${newUrl}?${query}`
+  } else {
+    return newUrl
+  }
+}
+
+const [,,...args] = process.argv
+start(args)
